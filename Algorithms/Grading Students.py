95a5ef699a9bd502e576eb12bf1a5ebfdb13e6b5
osadly <osadly@gmail.com>
2023-04-27 15:18:33 -0400

    Add files via upload

---
+++ Algorithms/Grading Students.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades):
+    grades2=grades
+    for i in range(len(grades)):
+        grd=grades[i]
+        if grd >= 38:
+            nextMul5=int(((grd//5)+1)*5)
+            if nextMul5-grd < 3:
+                grades2[i]=nextMul5
+                
+    return grades2
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
