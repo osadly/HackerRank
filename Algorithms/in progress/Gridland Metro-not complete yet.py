5400bfc92bd71a9ed2ffb90c8ca832323f90b535
osadly <osadly@gmail.com>
2023-06-03 22:19:03 -0400

    Update Gridland Metro-not complete yet.py

--- Algorithms/in progress/Gridland Metro-not complete yet.py
+++ Algorithms/in progress/Gridland Metro-not complete yet.py
@@ -1,56 +1,37 @@
-#!/bin/python3
-
 import math
 import os
 import random
 import re
 import sys
 
-#
-# Complete the 'gridlandMetro' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts following parameters:
-#  1. INTEGER n
-#  2. INTEGER m
-#  3. INTEGER k
-#  4. 2D_INTEGER_ARRAY track
-#
-
 def gridlandMetro(n, m, k, track):
     #ret=m*(n-k)
     ret=0
     trks=[]
+    dictRows_Trks = {}
     rowsWthTrksLst_ALL=[]
-    dictRows_TrksCnt = {}
     for i in range(k):
-        rowsWthTrksLst_ALL.add(track[i][0])
-        dictRows_TrksCnt[track[i][0]]+=1
+        #rowsWthTrksLst_ALL.add(track[i][0])
+        dictRows_Trks[track[i][0]-1].append([track[i][1]-1,track[i][2]-1])
     
-    rowsWthTrksLst_Unq = list(set(rowsWthTrksLst_ALL))
+    #rowsWthTrksLst_Unq = list(set(rowsWthTrksLst_ALL))
         
-    rowsWthMnyTrksLst=list(rowsWthMnyTrksSet)
-    rowsUsedForTrks=len(rowsWthMnyTrksLst)
-    ret=m*(n-rowsUsedForTrks)
+    #rowsWthMnyTrksLst=list(rowsWthMnyTrksSet)
+    #rowsUsedForTrks=len(rowsWthMnyTrksLst)
+    #ret=m*(n-rowsUsedForTrks)
+    print(dictRows_Trks)
+    return ret
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     first_multiple_input = input().rstrip().split()
-
     n = int(first_multiple_input[0])
-
     m = int(first_multiple_input[1])
-
     k = int(first_multiple_input[2])
-
     track = []
-
     for _ in range(k):
         track.append(list(map(int, input().rstrip().split())))
 
     result = gridlandMetro(n, m, k, track)
-
     fptr.write(str(result) + '\n')
-
     fptr.close()
