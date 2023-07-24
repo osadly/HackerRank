9dfe972b53f67ccd5f4295fa6f6cbd11c7fb677f
osadly <osadly@gmail.com>
2023-07-24 00:36:00 -0400

    Add files via upload

---
+++ Algorithms/Sequence Equation.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'permutationEquation' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY p as parameter.
+#
+
+def permutationEquation(p):
+    n=len(p)
+    ret=[0]*n
+    dct = {}
+    for i in range(n):
+        dct[p[i]]=i+1
+    
+    for i in range(n):
+        ret[i]=dct[dct[i+1]]
+    print(ret)
+    return ret
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    p = list(map(int, input().rstrip().split()))
+
+    result = permutationEquation(p)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
