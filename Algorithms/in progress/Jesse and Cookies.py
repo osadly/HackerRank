5fda46c89376fd837acf94dad9a14b78c8a40d61
osadly <osadly@gmail.com>
2023-07-25 00:21:19 -0400

    Add files via upload

---
+++ Algorithms/in progress/Jesse and Cookies.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'cookies' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. INTEGER_ARRAY A
+#
+
+def cookies(k, A):
+    A2=sorted(A)
+    cnt=0
+    mn=A2[0]
+    i=0
+    while mn <= k:
+        x=A2[i]+A2[i+1]
+        A2.remove(A[i])
+        A2.remove(A[i+1])
+        
+        cnt+=1
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    A = list(map(int, input().rstrip().split()))
+
+    result = cookies(k, A)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
