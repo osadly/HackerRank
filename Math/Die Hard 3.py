e8ba09d43f34a7ea1e4f995ef44580c4684fedf5
osadly <osadly@gmail.com>
2023-07-26 09:05:03 -0400

    Add files via upload

---
+++ Math/Die Hard 3.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'solve' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. INTEGER a
+#  2. INTEGER b
+#  3. INTEGER c
+#
+
+def solve(a, b, c):
+    if c==a or c==b or (c<max(a,b) and c % math.gcd(a,b)==0):
+        return "YES"
+    return "NO"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        a = int(first_multiple_input[0])
+
+        b = int(first_multiple_input[1])
+
+        c = int(first_multiple_input[2])
+
+        result = solve(a, b, c)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
