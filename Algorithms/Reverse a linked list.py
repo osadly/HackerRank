5aaff6dc06489707fab87a69ee49214305d4147e
osadly <osadly@gmail.com>
2023-08-24 12:52:14 -0400

    Add files via upload

---
+++ Algorithms/Reverse a linked list.py
@@ -0,0 +1,74 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+def reverse(llist):
+    arr=[]
+    while llist!=None:
+        arr.append(llist.data)
+        llist=llist.next
+    print(arr)
+    
+    ret=SinglyLinkedList()
+    for i in range(len(arr)-1,-1,-1):
+        llist_item = arr[i]
+        ret.insert_node(llist_item)
+    
+    return ret.head
+
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    tests = int(input())
+
+    for tests_itr in range(tests):
+        llist_count = int(input())
+
+        llist = SinglyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        llist1 = reverse(llist.head)
+
+        print_singly_linked_list(llist1, ' ', fptr)
+        fptr.write('\n')
+
+    fptr.close()
